bc4a6c7525b387b0d69871c4d4fc063310f5793f
Muhammad Asim Siddique <muhammad.asim.siddique@kyndryl.com>
2022-03-17 01:09:45 +0500

    Fetch Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OverlayDataService } from './services/overlay-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'graveyard-viewer';
+
+  constructor(private overlayDataService: OverlayDataService) { }
+
+  ngOnInit(): void {
+    this.overlayDataService.getData()
+      .subscribe((data: any) => console.log(data));
+  }
 }
